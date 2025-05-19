fd5d2c27788daa4779b0a6e9545e6c892386e75d
spartanis119 <camargobarajasdanielfelipe72@gmail.com>
2025-05-19 03:10:14 +0000

    creating routes and using new redirect method

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    // Si queremos redireccionar a una ruta lo podemos realizar de dos maneras pasandole la ruta relativa o usar el nombre de una ruta
+    // Para el segundo caso es que es el que mas se usa actualmente es mejor usar el metodo to_route() que es lo mismo que un redirect()->route('') pero mas simplificado
+    return to_route('contact2');
+})->name('contact');
+
+Route::get('/contact2', function () {
+    return view('testing_routes/contact2');
+})->name('contact2');
